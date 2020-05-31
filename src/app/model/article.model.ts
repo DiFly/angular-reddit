b30da36a8fa59361b3cf13cb84e29ebf2cf9cc15
difly <kryshen.yaroslav@gmail.com>
2020-05-31 23:48:09 +0300

    view domain string from url string

--- src/app/model/article.model.ts
+++ src/app/model/article.model.ts
@@ -2,12 +2,19 @@ export class Article {
   votes: number;
   title: string;
   link: string;
-  domain: string;
 
   constructor(title: string, link: string, votes?: number) {
     this.title = title;
     this.link = link;
     this.votes = votes || 0;
-    this.domain = 'testdomain';
+  }
+
+  domain(): string {
+    try {
+      const domainAndPath: string = this.link.split('//')[1];
+      return domainAndPath.split('/')[0];
+    } catch (err) {
+      return null;
+    }
   }
 }
