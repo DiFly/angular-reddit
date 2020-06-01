5802ef0d4ef8812731a00e062e645d047e403dfe
difly <kryshen.yaroslav@gmail.com>
2020-06-01 12:31:58 +0300

    encapsulate voteUp and vodeDown logic in class model

--- src/app/model/article.model.ts
+++ src/app/model/article.model.ts
@@ -9,6 +9,14 @@ export class Article {
     this.votes = votes || 0;
   }
 
+  voteUp(): void {
+    this.votes += 1;
+  }
+
+  voteDown(): void {
+    this.votes -+ 1;
+  }
+
   domain(): string {
     try {
       const domainAndPath: string = this.link.split('//')[1];
